33a6bc7431f224ffc485ca61baef98df74b5d896
Cypher <Cypher0601@gmail.com>
2020-06-30 19:47:36 +0800

    add restaurant graph

--- src/main/resources/static/js/restaurant.js
+++ src/main/resources/static/js/restaurant.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
             var dataset = res.content;
             var ctx = document.getElementById("restaurantDailySales");
             var num = Object.values(dataset);
-            alert(num.length);
             var sum = [];
             for (i = 0; i < num.length; i++){
                 var num2 = Object.values(num[i]);
@@ -16,7 +15,6 @@ $(document).ready(function() {
                 }
                 sum.push(summary);
             }
-            console.log(sum);
             var myChart = new Chart(ctx, {
                 type: 'line',
                 data: {
@@ -58,49 +56,65 @@ $(document).ready(function() {
                 }
             });
         })
-    // getRequest("/restaurant/getWeeklySales",
-    //     function (res) {
-    //         var dataset = res.content;
-    //         var ctx = document.getElementById("restaurantWeeklySales");
-    //         var myChart = new Chart(ctx, {
-    //             type: 'line',
-    //             data: {
-    //                 labels: tableLabels,
-    //                 datasets: [{
-    //                     label: '商家销量',
-    //                     data: tableData,
-    //                     backgroundColor: window.chartColors.blue,
-    //                     borderColor: window.chartColors.blue,
-    //                     fill: false
-    //                 }]
-    //             },
-    //             options: {
-    //                 responsive: true,
-    //                 title: {
-    //                     display: true,
-    //                     text: '商家周销表'
-    //                 },
-    //                 hover: {
-    //                     mode: 'nearest',
-    //                     intersect: true
-    //                 },
-    //                 scales: {
-    //                     xAxes: [{
-    //                         display: true,
-    //                         scaleLabel: {
-    //                             display: true,
-    //                             labelString: '日期'
-    //                         }
-    //                     }],
-    //                     yAxes: [{
-    //                         display: true,
-    //                         scaleLabel: {
-    //                             display: true,
-    //                             labelString: '销量'
-    //                         }
-    //                     }]
-    //                 }
-    //             }
-    //         });
-    //     })
+    getRequest("/restaurant/getWeeklySales",
+        function (res) {
+            var dataset = res.content;
+            var ctx = document.getElementById("restaurantWeeklySales");
+            var num = Object.values(dataset);
+            var sum = [];
+            for (i = 0; i < num.length; i++){
+                var num2 = Object.values(num[i]);
+
+                var summary = 0;
+                for(j = 0; j < num2.length; j++){
+                    summary += num2[j].num;
+                }
+                sum.push(summary);
+            }
+            var date = Object.keys(dataset);
+            var dateset = [];
+            for(i = 0;i < date.length; i++){
+                dateset.push(date[i].substring(0,10));
+            }
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: dateset,
+                    datasets: [{
+                        label: '商家销量',
+                        data: sum,
+                        backgroundColor: window.chartColors.blue,
+                        borderColor: window.chartColors.red,
+                        fill: false
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: '商家周销表'
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: '周起始日期'
+                            }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: '销量'
+                            }
+                        }]
+                    }
+                }
+            });
+        })
 })
